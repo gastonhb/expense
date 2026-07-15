be30594e9ed7d1f80edbe6e3af1f64c509acd930
gastonhb <gaston.herrerabaron@outlook.com>
2026-07-15 12:09:05 -0300

    add incometypevalidation

---
+++ src/validations/incomeType.validation.js
@@ -0,0 +1,53 @@
+const Joi = require('joi');
+const { paginationAndOrder } = require('./pagination.validation');
+const { uuid, orderString } = require('./custom.validation');
+
+const find = {
+  query: Joi.object().keys({
+    ...paginationAndOrder,
+    code: Joi.string().trim(),
+    name: Joi.string().trim(),
+    _order: Joi.string().custom(orderString)
+  })
+};
+
+const create = {
+  body: Joi.object().keys({
+    name: Joi.string().trim().required(),
+    description: Joi.string().trim(),
+    color: Joi.string().trim(),
+    icon: Joi.string().trim()
+  })
+};
+
+const findById = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(uuid).required()
+  })
+};
+
+const update = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(uuid).required()
+  }),
+  body: Joi.object().keys({
+    name: Joi.string().trim(),
+    description: Joi.string().trim(),
+    color: Joi.string().trim(),
+    icon: Joi.string().trim()
+  }).min(1)
+};
+
+const destroy = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(uuid).required()
+  })
+};
+
+module.exports = {
+  find,
+  create,
+  findById,
+  update,
+  destroy
+};
